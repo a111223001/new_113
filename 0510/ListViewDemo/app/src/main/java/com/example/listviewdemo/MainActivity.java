0aebb2a632c2048b0eb24bae5b028f4889dcdb95
a111223001 <A111223001@mail.shu.edu.tw>
2024-05-10 14:59:56 +0800

    113-05-10

---
+++ 0510/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lv = (ListView) findViewById(R.id.listview);
+    private String[] cities;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cities = getResources().getStringArray(R.array.cities);
+
+        ArrayAdapter<String> a = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, cities);
+
+        lv =(ListView) findViewById(R.id.listview);
+        lv.setAdapter(a);
+        lv.setOnClickListener(this);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+                output.setText("你是住在" + cities[position]);
+            }
+        });
+
+    }
+}
